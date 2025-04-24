f42dd01690edabc6cfe4d9429c20afc17703449c
mohamedobaya <mohamed.ali.obaya@gmail.com>
2025-04-24 19:08:57 +0200

    Initial Commit

---
+++ iConverse/iConverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConverseApp.swift
+//  iConverse
+//
+//  Created by Mohamed Obaya on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct iConverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
